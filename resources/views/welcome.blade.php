37438f38371a39a5a34f6a37c54969533778ea88
Gabrielkphiri <148223614+Gabrielkphiri@users.noreply.github.com>
2023-11-08 16:15:14 +0200

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <link rel="stylesheet"
+          href="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/jqvmap/jqvmap.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css?v=3.2.0')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/daterangepicker/daterangepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/summernote/summernote-bs4.min.css')}}">
+    <script nonce="1215ee1b-49de-4eec-8e01-d5f5765a1708">(function (w, d) {
+            !function (bb, bc, bd, be) {
+                bb[bd] = bb[bd] || {};
+                bb[bd].executed = [];
+                bb.zaraz = {deferred: [], listeners: []};
+                bb.zaraz.q = [];
+                bb.zaraz._f = function (bf) {
+                    return async function () {
+                        var bg = Array.prototype.slice.call(arguments);
+                        bb.zaraz.q.push({m: bf, a: bg})
+                    }
+                };
+                for (const bh of ["track", "set", "debug"]) bb.zaraz[bh] = bb.zaraz._f(bh);
+                bb.zaraz.init = () => {
+                    var bi = bc.getElementsByTagName(be)[0], bj = bc.createElement(be),
+                        bk = bc.getElementsByTagName("title")[0];
+                    bk && (bb[bd].t = bc.getElementsByTagName("title")[0].text);
+                    bb[bd].x = Math.random();
+                    bb[bd].w = bb.screen.width;
+                    bb[bd].h = bb.screen.height;
+                    bb[bd].j = bb.innerHeight;
+                    bb[bd].e = bb.innerWidth;
+                    bb[bd].l = bb.location.href;
+                    bb[bd].r = bc.referrer;
+                    bb[bd].k = bb.screen.colorDepth;
+                    bb[bd].n = bc.characterSet;
+                    bb[bd].o = (new Date).getTimezoneOffset();
+                    if (bb.dataLayer) for (const bo of Object.entries(Object.entries(dataLayer).reduce(((bp, bq) => ({...bp[1], ...bq[1]})), {}))) zaraz.set(bo[0], bo[1], {scope: "page"});
+                    bb[bd].q = [];
+                    for (; bb.zaraz.q.length;) {
+                        const br = bb.zaraz.q.shift();
+                        bb[bd].q.push(br)
+                    }
+                    bj.defer = !0;
+                    for (const bs of [localStorage, sessionStorage]) Object.keys(bs || {}).filter((bu => bu.startsWith("_zaraz_"))).forEach((bt => {
+                        try {
+                            bb[bd]["z_" + bt.slice(7)] = JSON.parse(bs.getItem(bt))
+                        } catch {
+                            bb[bd]["z_" + bt.slice(7)] = bs.getItem(bt)
+                        }
+                    }));
+                    bj.referrerPolicy = "origin";
+                    bj.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(bb[bd])));
+                    bi.parentNode.insertBefore(bj, bi)
+                };
+                ["complete", "interactive"].includes(bc.readyState) ? zaraz.init() : bb.addEventListener("DOMContentLoaded", zaraz.init)
+            }(w, d, "zarazData", "script");
+        })(window, document);</script>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    @include('layouts.nav')
+
+
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+        <a href="index3.html" class="brand-link">
+            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                 style="opacity: .8">
+            <span class="brand-text font-weight-light">AdminLTE 3</span>
+        </a>
+
+        @include('layouts.sidebar')
+
+    </aside>
+
+    <div class="content-wrapper">
+
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-lg-3 col-6">
+
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+                                <p>New Orders</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                <p>Bounce Rate</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>44</h3>
+                                <p>User Registrations</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>65</h3>
+                                <p>Unique Visitors</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="row">
+
+                    <section class="col-lg-12 connectedSortable">
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    <i class="fas fa-chart-pie mr-1"></i>
+                                    Sales
+                                </h3>
+                                <div class="card-tools">
+                                    <ul class="nav nav-pills ml-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <form role="form">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputMan_on">Man_on</label>
+                                                <input type="number" class="form-control" id="exampleInputMan_on"
+                                                       placeholder=" Enter Man_on">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputUsername">Username</label>
+                                                <input type="text" class="form-control" id="exampleInputUsername"
+                                                       placeholder="Enter Username">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputFullname">Full Name</label>
+                                                <input type="text" class="form-control" id="exampleInputFull_Name"
+                                                       placeholder="Enter Full_Name">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmployeeDesignation">Employee designation</label>
+                                                <input type="date" class="form-control" id="exampleInputEmployessdesignation"
+                                                       placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputarea">Area</label>
+                                                <input type="text" class="form-control" id="exampleInputarea"
+                                                       placeholder="Enter area">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputreviewaction">Review Action</label>
+                                                <input type="text" class="form-control" id="exampleInputReviewAction"
+                                                       placeholder="">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">System</label>
+                                                    <input type="text" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Role</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Comments</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </section>
+
+                </div>
+
+                <div class="row">
+
+                    <section class="col-lg-12 connectedSortable">
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    <i class="fas fa-chart-pie mr-1"></i>
+                                    Sales
+                                </h3>
+                                <div class="card-tools">
+                                    <ul class="nav nav-pills ml-auto">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <form role="form">
+                                <div class="card-body">
+                                    <table id="example2" class="table table-bordered table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>Reviewed By</th>
+                                            <th>Man no</th>
+                                            <th>Approved By HOD</th>
+                                            <th>System Administrtor</th>
+                                            <th>Approval date</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td>
+                                            </td>
+                                            <td></td>
+                                            <td> </td>
+                                            <td></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </section>
+
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+
+    @include('layouts.footer')
+
+    <aside class="control-sidebar control-sidebar-dark">
+
+    </aside>
+
+</div>
+
+
+<script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+
+<script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/sparklines/sparkline.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/daterangepicker/daterangepicker.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/summernote/summernote-bs4.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+
+<script src="{{asset('adminlte/dist/js/adminlte.js?v=3.2.0')}}"></script>
+
+<script src="{{asset('adminlte/dist/js/demo.js')}}"></script>
+
+<script src="{{asset('adminlte/dist/js/pages/dashboard.js')}}"></script>
+</body>
+</html>
